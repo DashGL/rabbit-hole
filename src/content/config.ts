5b98a58f72ab29f0262ab03b6cfb5ac26a7be6b6
kion <kion@dashgl.com>
2024-02-26 16:51:36 +0900

    proof of concept blog post

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+    type: 'content', // v2.5.0 and later
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string(),
+        publishDate: z.date(),
+        category: z.enum(['Hardware', 'Weebits']),
+        heroImage: image(),
+    }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+    'blog': blogCollection,
+};
