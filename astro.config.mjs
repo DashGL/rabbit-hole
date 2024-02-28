60da518670b3de199d8536e10129f93e51160383
kion <kion@dashgl.com>
2024-02-28 16:47:59 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dashgl.github.io',
   base: '/rabbit-hole',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), sitemap()]
 });
