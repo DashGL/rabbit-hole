1561bf887803460bed1248ab59f07b9e618f8301
kion <kion@dashgl.com>
2024-03-23 13:30:01 +0900

    use .mdx for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dashgl.github.io',
   base: '/rabbit-hole',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt()]
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), mdx()]
 });
