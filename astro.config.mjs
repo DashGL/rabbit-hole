c0fbdb911a03e7a0be9930a0c990376aa842d3c2
Kion <kion@dashgl.com>
2024-08-04 10:41:01 +0900

    Domain (#36)
    * update scenes
    
    * update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dashgl.github.io',
-  base: '/rabbit-hole',
-  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), mdx()]
+  site: "https://dashgl.com",
+  base: "/",
+  integrations: [tailwind(), icon(), sitemap(), robotsTxt(), mdx()],
 });
